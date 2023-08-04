fff5080799176fcc44fabad01fe2815ea0b97dfd
Zheng Kai <zhengkai@gmail.com>
2023-08-04 14:15:15 +0800

    up ua

--- fetch.go
+++ fetch.go
@@ -6,7 +6,7 @@ import (
 )
 
 // UserAgent for FetchURL
-var UserAgent = `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.70`
+var UserAgent = `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.188`
 
 // FetchURL ...
 func FetchURL(url string) (ab []byte, err error) {
